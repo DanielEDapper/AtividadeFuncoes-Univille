3388b7dee00b3c3839d0a0e3136d60e5ca2b6557
DANIEL ERTHAL DAPPER <daniel.dapper@univille.br>
2026-09-15 11:12:39 -0300

    Feat: Implementa Atividade - Sem Testes

---
+++ script.py
@@ -0,0 +1,172 @@
+import time
+
+opcoes = []
+votos = []
+
+def iniciarPrograma():
+    while True:
+        print("""
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                ENQUETES                ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+1 - Cadastrar Opção
+2 - Listar Opções
+3 - Registrar Voto na Enquete
+4 - Consultar Quantidade de Votos
+5 - Mostrar Resultado
+6 - Mostrar Opção Vencedora
+7 - Encerrar Programa""")
+
+        opcao = int(input("O que deseja fazer?: "))
+
+        match opcao:
+            case 1:
+                limpaTerminal()
+                cadastrarOpcao()
+
+                time.sleep(5)
+            case 2:
+                limpaTerminal()
+                listarOpcao()
+
+                time.sleep(5)
+            case 3:
+                registrarVoto()
+            case 4:
+                consultarQuantidadeVoto()
+            case 5:
+                mostrarResultado()
+            case 6:
+                mostrarOpcaoVencedor()
+            case 7:
+                print("""
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃       PROGRAMA DE VOTOS ENCERRADO      ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+                break
+            case _:
+                print("Opção invalida!")
+
+def cadastrarOpcao():
+    opcao = str(input("Digite a opção: "))
+
+    if opcao not in opcoes:
+        opcoes.append(opcao)
+        votos.append(0)
+
+        print("Opção cadastrada com sucesso!")
+
+def listarOpcao():
+    print("""
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃           OPÇÕES CADASTRADAS           ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛""")
+
+    if len(opcoes) == 0:
+        print("Nenhuma opção cadastrada.")
+
+    for i in range(len(opcoes)):
+        print(f"{i + 1}. {opcoes[i]}")
+
+def registrarVoto():
+    if len(opcoes) == 0:
+        print("Nenhuma opção registrada.")
+
+    listarOpcao()
+
+    try:
+        escolha = int(input("Digite o número da opção: "))
+
+        if escolha < 1 or escolha > len(opcoes):
+            print("Opção inválida!")
+            return
+
+        votos[escolha-1] += 1
+        print("Votos registrados com sucesso!")
+
+    except ValueError:
+        print("Digite apenas números.")
+
+def consultarQuantidadeVoto():
+    if len(opcoes) == 0:
+        print("Nenhuma opção registrada.")
+
+    for i in range(len(opcoes)):
+        print(f"{i + 1}. {opcoes[i]}")
+
+def mostrarResultado():
+    if len(opcoes) == 0:
+        print("Nenhuma opção registrada.")
+
+    totalVotos = sum(votos)
+
+    for i in range(len(opcoes)):
+        quantidade = votos[i]
+
+        if totalVotos == 0:
+            percentualVoto = 0
+        else:
+            percentualVoto = (quantidade / totalVotos) * 100
+
+        print(
+            f"{opcoes[i]}:"
+            f"{quantidade} voto(s) - "
+            f"{percentualVoto}"
+        )
+
+def mostrarOpcaoVencedor():
+    if len(opcoes) == 0:
+        print("Nenhuma opção registrada.")
+
+    maiorQuantidade = max(votos)
+
+    if maiorQuantidade == 0:
+        print("Ainda não existem votos!")
+        return
+
+    vencedores = []
+
+    for i in range(len(opcoes)):
+        if votos[i] == maiorQuantidade:
+            vencedores.append(opcoes[i])
+
+    if len(vencedores) > 1:
+        print("EMPATE")
+
+        for opcao in vencedores:
+            print(f"- {opcao}")
+
+
+    else:
+        print("Opção Vencedora")
+        print(f"Vencedora: {vencedores[0]}")
+        print(f"Quantidade de Votos: {maiorQuantidade}")
+
+def limpaTerminal():
+    print("""
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """)
+
+
+iniciarPrograma()
